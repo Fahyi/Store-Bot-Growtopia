4dec9ea74b85e48a1d5ca91bc62bd8149d785c9c
Fahyi <59529612+Fahyi@users.noreply.github.com>
2023-07-12 19:06:55 +0700

    Delete suggestion.js

--- schemas/suggestion.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const sugesti = mongoose.Schema({
-    userId: [],
-    saran: String
-})
-
-const Suggest = mongoose.model("suggestions", sugesti)
-module.exports = Suggest
